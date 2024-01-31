9dda85b85aeaeadff4b39d3278dccf6327ddf8b5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-31 14:48:45 +0200

    add app/api

---
+++ src/app/api/greet/route.ts
@@ -0,0 +1,21 @@
+import { Contact } from "@/models/contacts";
+import { NextResponse, type NextRequest } from "next/server";
+// import mongoose from 'mongoose';
+
+export async function GET(req: NextRequest) {
+  console.log("TEST)))))))))()((((");
+  // const requestHeaders = new Headers(req.headers)
+  // console.log("handler  headers::::::::::::::::", requestHeaders.get('cache-control'));
+
+  const url = req.url;
+
+  //   const newContact = {
+  //     name: "Petr",
+  //     age: 21,
+  //   };
+
+  //   const contact = await Contact.create(newContact);
+  //   console.log("GET  contact:", contact);
+
+  return NextResponse.json({ data: "contact" });
+}
