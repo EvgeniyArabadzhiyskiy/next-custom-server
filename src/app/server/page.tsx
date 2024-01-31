26b4e9a819cc610e8721d10767dcdc89a7a8d06c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-01-31 17:36:29 +0200

    add files

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -3,23 +3,23 @@ import { Contact } from "@/models/contacts";
 import Link from "next/link";
 
 const getContact = async () => {
-  await connectDB();
-  
+//   await connectDB();
 
-  const contacts = await Contact.find();
-  return contacts
+
+//   const contacts = await Contact.find();
+  return "contacts"
 };
 
 const Server = async () => {
    const contacts = await getContact()
-   const mango = contacts[0].name
-   console.log("Server  mango:", mango);
+//    const mango = contacts[0].name
+//    console.log("Server  mango:", mango);
    
   return (
     <>
       <Link href={"/"}>Home</Link>
       <h1>
-        {mango}
+        {contacts}
       </h1>
     </>
   );
