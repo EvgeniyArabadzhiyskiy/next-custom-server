b5a946c88bb71f5bfbc3edc95fad768b243eb45e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-02-21 21:39:14 +0200

    add prisma

---
+++ prisma/prismaClient.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+// export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// Docs about instantiating `PrismaClient` with Next.js:
+// https://pris.ly/d/help/next-js-best-practices
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!(global as any).prisma) {
+//     (global as any).prisma = new PrismaClient();
+//   }
+//   prisma = (global as any).prisma;
+// }
+
+// export default prisma;
